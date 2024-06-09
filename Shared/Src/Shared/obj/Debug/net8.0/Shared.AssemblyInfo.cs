dde6f008a1dadddb08ab35fb64300a319a269556
DotNetAbuser <dotnetabuser@gmail.com>
2024-06-09 23:58:12 +0300

    ver 2

--- Shared/Src/Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
+++ Shared/Src/Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d2480f9de92f1fc8dba9da7f6618f9b7d66a6a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71c7d7fb859d2b1ae22a01f923bae889502d0af9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
